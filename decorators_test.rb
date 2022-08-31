e23f3210dbe95fe45187744e76f9fd222aacf66a
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-01 01:12:19 +0300

    Fix linters errors

--- decorators_test.rb
+++ decorators_test.rb
@@ -3,8 +3,8 @@ require_relative './capitalize_decorator'
 require_relative './trimmer_decorator'
 
 person = Person.new(22, name: 'maximilianus')
-  person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
-  capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  capitalizedTrimmedPerson.correct_name
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
