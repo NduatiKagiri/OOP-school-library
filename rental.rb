f46c11ff3d0b8f7ebbdcf088f23c69eaa0e019fa
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-01 23:42:14 +0300

    Add Book and Rental relationship

--- rental.rb
+++ rental.rb
@@ -1,7 +1,15 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book
 
   def initialize(date)
     @date = date
+    @book = book
+    @rentals = []
+    belongs_to
+  end
+
+  def belongs_to
+    @rentals.push(self)
+    book.rentals.push(self) unless book.rentals.include?(self)
   end
 end
