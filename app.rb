1c1347052b3179c2cf27192ff6433d8fad8d6f04
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-09-02 21:07:07 +0300

    Add functions to create and list books

--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
+require_relative './book'
+
 class App
   def initialize
     @books_list = []
     @people_list = []
     @rentals_list = []
+  end
+
+  def create_book
+    print '   Enter Book Title: '
+    title = gets.chomp
+    print '   Enter Book author: '
+    author = gets.chomp
+
+    new_book = Book.new(title.capitalize, author.capitalize)
+
+    @books_list.push(new_book)
+  end
+
+  def list_books
+    @books_list.each do |book|
+      puts "Book: #{book.title}            Author: #{book.author}"
+    end
   end
 end
